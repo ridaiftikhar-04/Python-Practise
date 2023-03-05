afeef7f9a8fe4a3a07fb606041ed97d42cd25d1c
Rida <ridaiftikhar88@gmail.com>
2023-03-05 23:29:27 +0500

    first commit

--- Python_practise.py
+++ Python_practise.py
@@ -1,4 +1,12 @@
+#practise
+from tkinter import Y
+
+
 print("Hello World")
 
-x = input("Whats your name")
+x = input("Whats your name: ")
 print("Hi!" , x)
+
+
+y = input("Whats your DOB:")
+print(y)
