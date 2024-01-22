f81f05bb06bb3dd931c4769b8fa855f7749106c8
Rida Iftikhar <ridaiftikhar88@gmail.com>
2024-01-23 00:29:17 +0500

    Lists

--- Python_practise.py
+++ Python_practise.py
@@ -9,6 +9,32 @@ print(list1)
 list1.insert(2 ,"Korean")
 print(list1)
 
+#add another list at the end of a list
 list2 = ['Spanish', 'Japanese']
 list1.extend(list2)
 print(list1)
+
+#remove and return last element of the list
+list2.pop()
+print(list2)
+
+#sort the list alphabetically or in ascending order
+list1.sort()
+print(list1)
+
+#sort the list in descending order
+list1.sort(reverse = True)
+print(list1)
+
+#loop through the list
+for index, course in enumerate(list1):
+    print(index , course)
+
+#comma separated list
+list3 = ','.join(list1)
+print(list3)
+
+#split a string and convert each word into list element
+split_str = "Split this string into list elements"
+new_list = split_str.split(" ")
+print(new_list)
