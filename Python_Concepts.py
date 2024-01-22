f2ee07265b07d14543a336ea91237254a7bbad69
Rida Iftikhar <ridaiftikhar88@gmail.com>
2024-01-22 23:14:08 +0500

    Tuples

--- Python_Concepts.py
+++ Python_Concepts.py
@@ -84,3 +84,14 @@ else:
 print(f'Down Payment: ${down_payment}')
 
 #Lists, Tuples, Sets, Dictionaries
+
+tuples = ('History' , 'Maths' , 'Art')
+
+#slicing
+#remove first item of the tuple and display the rest
+tuples1 = tuples[1:] 
+print(tuples1)
+
+tuples2 = tuples[:2]
+print(tuples2)
+
