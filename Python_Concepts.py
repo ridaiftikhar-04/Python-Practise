aa1d74f06c62034a55a178977b866db98dc8348c
Rida Iftikhar <ridaiftikhar88@gmail.com>
2024-01-27 23:08:43 +0500

    Functions

--- Python_Concepts.py
+++ Python_Concepts.py
@@ -178,3 +178,23 @@ while x<10:
 # while True:
 #     print(y)
 #     y += 1
+    
+#Functions
+def hello_func(greeting, name = 'You'):
+    return '{} , {}'.format(greeting, name)
+
+print(hello_func("Hi"))
+
+
+#Function that accepts arbitrary number of values using * and **
+def student_info(*args, **kwargs): 
+    print(args)
+    print(kwargs)
+
+subjects = ('Maths', 'Art')
+info = {'name' : 'John' , 'age' : 24}
+
+student_info(*subjects, **info)
+
+#Doc string: a string like """ test doc """ in a function explaining what the function does
+
